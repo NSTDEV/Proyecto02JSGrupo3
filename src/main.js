01195477391417510ee4f37a0994cc3200cc9134
Ulises Toconas <toconasdanielulises@gmail.com>
2023-09-20 00:58:39 -0300

    se aumenta un select para supermercados y categorias para una facil eleccion y listar precios mas economicos

--- src/main.js
+++ src/main.js
@@ -1,95 +1,3 @@
-/**const listaDeProductos = [
-    {
-        "nombre": "Arroz x1Kg",
-        "precio": 500.99,
-        "supermercado": "Vea",
-        "categoria": "Alimentos secos"
-    },
-    {
-        "nombre": "Leche x1L",
-        "precio": 380.50,
-        "supermercado": "Comodín",
-        "categoria": "Lácteos"
-    },
-    {
-        "nombre": "Pan",
-        "precio": 500.00,
-        "supermercado": "Disco",
-        "categoria": "Panadería"
-    },
-    {
-        "nombre": "Manzanas",
-        "precio": 690.90,
-        "supermercado": "Vea",
-        "categoria": "Frutas y verduras"
-    },
-    {
-        "nombre": "Detergente x500ml",
-        "precio": 415.75,
-        "supermercado": "Comodín",
-        "categoria": "Limpieza"
-    },
-    {
-        "nombre": "Carne de res x1Kg",
-        "precio": 2500.00,
-        "supermercado": "Vea",
-        "categoria": "Carnes"
-    },
-    {
-        "nombre": "Aceite de oliva x900ml",
-        "precio": 1200.00,
-        "supermercado": "Comodín",
-        "categoria": "Aceites y condimentos"
-    },
-    {
-        "nombre": "Yogur x900ml",
-        "precio": 450.50,
-        "supermercado": "Disco",
-        "categoria": "Lácteos"
-    },
-    {
-        "nombre": "Cerveza x1L",
-        "precio": 580.00,
-        "supermercado": "Obrero",
-        "categoria": "Bebidas alcohólicas"
-    },
-    {
-        "nombre": "Papel higiénico x4/u",
-        "precio": 520.99,
-        "supermercado": "Chango Más",
-        "categoria": "Higiene"
-    },
-    {
-        "nombre": "Huevos x12/u",
-        "precio": 470.00,
-        "supermercado": "Carrefour",
-        "categoria": "Huevos y lácteos"
-    },
-    {
-        "nombre": "Fideos x500g",
-        "precio": 219.95,
-        "supermercado": "Vea",
-        "categoria": "Alimentos secos"
-    },
-    {
-        "nombre": "Jabón de manos x400ml",
-        "precio": 450.25,
-        "supermercado": "Comodín",
-        "categoria": "Limpieza"
-    },
-    {
-        "nombre": "Tomates x1Kg",
-        "precio": 450.00,
-        "supermercado": "Disco",
-        "categoria": "Frutas y verduras"
-    },
-    {
-        "nombre": "Queso x1Kg",
-        "precio": 2200.75,
-        "supermercado": "Vea",
-        "categoria": "Lácteos"
-    }
-];*/
 const listaDeProductos = [];
 
 let productosMasEconomicos = [];
@@ -111,9 +19,13 @@ function mostrarEnHTML(id, listaDeProductos) {
         const categoriaP = document.createElement('p');
         categoriaP.textContent = `Categoria: ${producto.categoria}`;
 
+        const superP =document.createElement('p');
+        superP.textContent=`Comercio: ${producto.supermercado}`;
+
         div.appendChild(nombreP);
         div.appendChild(precioP);
         div.appendChild(categoriaP);
+        div.appendChild(superP);
 
         lista.appendChild(div);
     });
@@ -126,8 +38,10 @@ function mostrarListaDeProductos() {
 function agregarProducto() {
     const nombre = document.getElementById('name').value;
     const precio = parseFloat(document.getElementById('price').value);
-    const supermercado = document.getElementById('market').value;
-    const categoria = document.getElementById('categorie').value;
+    let sup=document.getElementById('market');
+    const supermercado =sup.options[sup.selectedIndex].text; 
+    let cat=document.getElementById('categoria');
+    const categoria = cat.options[cat.selectedIndex].text;
 
     if (nombre && precio && supermercado && categoria) {
         const nuevoProducto = { "nombre": nombre, "precio": precio, "supermercado": supermercado, "categoria": categoria };
@@ -143,7 +57,7 @@ function agregarProducto() {
 
 function filtroMenorPrecio() {
     listaDeProductos.sort((producto, menor) => producto.precio - menor.precio);
-    productosMasEconomicos = listaDeProductos.slice(0, 5);
+    productosMasEconomicos = listaDeProductos.slice(0, 1);
 
     mostrarEnHTML('low-prices', productosMasEconomicos)
     alert("Estos son los productos más económicos: ");
