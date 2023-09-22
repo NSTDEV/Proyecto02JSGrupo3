068a626b4f607eaf295961e2e414ab3e2efe49c1
Nahuel Troncoso <92763369+NSTDEV@users.noreply.github.com>
2023-09-21 23:11:54 -0300

    Modificada la lógica de main.js, el index.html y el styles.css
    -main.js: Agregado lógica para cambiar de listados y agregar un nuevo producto, comentarios y agregada una pequeña lista como demostración.
    
    -Styles.css: Agregado estilos nuevos y modificadas algunas clases para tener un estilo uniforme.
    
    -index.html: Modificado para tener una mejor relación con la lógica y traer de manera más dinámica las listas.

--- src/main.js
+++ src/main.js
@@ -1,149 +1,131 @@
 const listaDeProductos = [
     {
-        "nombre": "Arroz x1Kg",
-        "precio": 500.99,
-        "supermercado": "Vea",
-        "categoria": "Alimentos secos"
-    },
-    {
-        "nombre": "Leche x1L",
-        "precio": 380.50,
-        "supermercado": "Comodín",
-        "categoria": "Lácteos"
-    },
-    {
-        "nombre": "Pan",
-        "precio": 500.00,
-        "supermercado": "Disco",
-        "categoria": "Panadería"
-    },
-    {
-        "nombre": "Manzanas",
-        "precio": 690.90,
-        "supermercado": "Vea",
-        "categoria": "Frutas y verduras"
-    },
-    {
-        "nombre": "Detergente x500ml",
-        "precio": 415.75,
-        "supermercado": "Comodín",
-        "categoria": "Limpieza"
-    },
-    {
-        "nombre": "Carne de res x1Kg",
-        "precio": 2500.00,
-        "supermercado": "Vea",
-        "categoria": "Carnes"
-    },
-    {
-        "nombre": "Aceite de oliva x900ml",
-        "precio": 1200.00,
-        "supermercado": "Comodín",
-        "categoria": "Aceites y condimentos"
-    },
-    {
-        "nombre": "Yogur x900ml",
-        "precio": 450.50,
-        "supermercado": "Disco",
-        "categoria": "Lácteos"
-    },
-    {
-        "nombre": "Cerveza x1L",
-        "precio": 580.00,
-        "supermercado": "Obrero",
-        "categoria": "Bebidas alcohólicas"
-    },
-    {
         "nombre": "Papel higiénico x4/u",
         "precio": 520.99,
-        "supermercado": "Chango Más",
+        "distribuidor": "Chango Más",
         "categoria": "Higiene"
     },
     {
         "nombre": "Huevos x12/u",
         "precio": 470.00,
-        "supermercado": "Carrefour",
-        "categoria": "Huevos y lácteos"
-    },
-    {
-        "nombre": "Fideos x500g",
-        "precio": 219.95,
-        "supermercado": "Vea",
-        "categoria": "Alimentos secos"
+        "distribuidor": "Carrefour",
+        "categoria": "Carnes / producto animal"
     },
     {
         "nombre": "Jabón de manos x400ml",
         "precio": 450.25,
-        "supermercado": "Comodín",
-        "categoria": "Limpieza"
+        "distribuidor": "Comodín",
+        "categoria": "Higiene"
     },
     {
         "nombre": "Tomates x1Kg",
         "precio": 450.00,
-        "supermercado": "Disco",
+        "distribuidor": "Jaguar",
         "categoria": "Frutas y verduras"
     },
     {
         "nombre": "Queso x1Kg",
         "precio": 2200.75,
-        "supermercado": "Vea",
-        "categoria": "Lácteos"
+        "distribuidor": "Vea",
+        "categoria": "Lacteos"
     }
 ];
 
-let productosMasEconomicos = [];
+let tituloDeLista = null;
 
-function mostrarEnHTML(id, listaDeProductos) {
+// Plantillas para mostrar diferentes tipos de listas (General o Baratos)
+function mostrarEnHTML(id, tituloSection, listaDeProductos) {
     const lista = document.getElementById(id);
+
+    // Si ya hay un título en la lista, no lo agregamos nuevamente
+    if (!tituloDeLista) {
+        const titulo = document.createElement('h2');
+        titulo.textContent = tituloSection;
+        titulo.classList.add('sub-title');
+        lista.parentNode.insertBefore(titulo, lista);
+        tituloDeLista = titulo;
+    } else {
+        tituloDeLista.textContent = tituloSection;
+    }
+
     lista.innerHTML = '';
 
     listaDeProductos.forEach(producto => {
-        const div = document.createElement('div');
-        div.className = 'product-container';
+        const contenedorDeProducto = document.createElement('div');
+        contenedorDeProducto.className = 'product-container';
 
-        const nombreP = document.createElement('p');
-        nombreP.textContent = `Nombre del producto: ${producto.nombre}`;
+        const nombre = document.createElement('p');
+        nombre.textContent = `Producto: ${producto.nombre}`;
 
-        const precioP = document.createElement('p');
-        precioP.textContent = `Precio: ${producto.precio} pesos`;
+        const precio = document.createElement('p');
+        precio.textContent = `Precio: ${producto.precio} pesos`;
 
-        const categoriaP = document.createElement('p');
-        categoriaP.textContent = `Categoria: ${producto.categoria}`;
+        const categoria = document.createElement('p');
+        categoria.textContent = `Categoria: ${producto.categoria}`;
 
-        div.appendChild(nombreP);
-        div.appendChild(precioP);
-        div.appendChild(categoriaP);
+        const distribuidor = document.createElement('p');
+        distribuidor.textContent = `Distribuidor: ${producto.distribuidor || ''}`;
 
-        lista.appendChild(div);
+        contenedorDeProducto.appendChild(nombre);
+        contenedorDeProducto.appendChild(precio);
+        contenedorDeProducto.appendChild(categoria);
+        contenedorDeProducto.appendChild(distribuidor);
+
+        lista.appendChild(contenedorDeProducto);
     });
 };
 
-function mostrarListaDeProductos() {
-    mostrarEnHTML('list-of-products', listaDeProductos);
-};
-
+// Funcion para agregar un producto nuevo teniendo como oblicación de llenar todos los campos
 function agregarProducto() {
     const nombre = document.getElementById('name').value;
     const precio = parseFloat(document.getElementById('price').value);
-    const supermercado = document.getElementById('market').value;
-    const categoria = document.getElementById('categorie').value;
+    let distribuidor = document.getElementById('market');
+    let categoria = document.getElementById('categorie');
 
-    if (nombre && precio && supermercado && categoria) {
-        const nuevoProducto = { "nombre": nombre, "precio": precio, "supermercado": supermercado, "categoria": categoria };
+    if (nombre && precio && distribuidor && categoria && distribuidor.selectedIndex > 0 && categoria.selectedIndex > 0) {
+        const distribuidorSel = distribuidor.options[distribuidor.selectedIndex].text;
+        const categoriaSel = categoria.options[categoria.selectedIndex].text;
+
+        const nuevoProducto = {
+            "nombre": nombre,
+            "precio": precio,
+            "distribuidor": distribuidorSel,
+            "categoria": categoriaSel
+        };
         listaDeProductos.push(nuevoProducto);
 
-        mostrarListaDeProductos()
         document.getElementById('product-form').reset();
         alert("¡Producto agregado correctamente!");
     } else {
         alert("Por favor, complete todos los campos.");
-    };
-};
+    }
+}
 
+// Filtro que devuelve los 3 productos más baratos
 function filtroMenorPrecio() {
-    listaDeProductos.sort((producto, menor) => producto.precio - menor.precio);
-    productosMasEconomicos = listaDeProductos.slice(0, 5);
+    ocultarListaDeProductos('product-list');
 
-    mostrarEnHTML('low-prices', productosMasEconomicos)
-    alert("Estos son los productos más económicos: ");
+    listaDeProductos.sort((productoA, productoB) => productoA.precio - productoB.precio);
+
+    mostrarEnHTML('product-list', 'Productos más económicos', listaDeProductos.slice(0, 3));
+    alert("Estos son los 3 productos más económicos");
+    listaVisible = true;
+}
+
+// Cambiar las listas para mejor visualización del usuario
+let listaVisible = true;
+
+function mostrarListaDeProductos() {
+    ocultarListaDeProductos('product-list');
+
+    mostrarEnHTML('product-list', 'Lista de Productos', listaDeProductos);
+    listaVisible = true;
+}
+
+function ocultarListaDeProductos(id) {
+    const lista = document.getElementById(id);
+
+    if (lista) {
+        lista.innerHTML = '';
+    }
 };
